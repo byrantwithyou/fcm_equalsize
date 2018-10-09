0b05bb8780a678aeefe65a59977b2b6f69aff369
byrantwithyou <byrantwithyou@163.com>
2018-10-10 05:58:56 +0800

    todo: condition for terminatin && check A, b

--- src/fcm_equalsize.py
+++ src/fcm_equalsize.py
@@ -6,7 +6,6 @@ import numpy as np
 import scipy
 import scipy.spatial as spatial
 from scipy import linalg
-from numpy import fromfunction
 
 
 def solve_beta(d_matrix):
@@ -20,48 +19,42 @@ def solve_beta(d_matrix):
   A = np.dot(foobar, np.repeat(np.eye(C), n, axis=0))
   bar = np.diag(np.sum(np.divide(1, 2 * d_matrix), axis=1))
   A = A - bar
-  return linalg.solve(A, b)
+  beta = linalg.solve(A, b)
+  return beta
 
 
 def solve_alpha(d_matrix, beta):
-  n = d_matrix.shape[1]
-  foo = np.divide(1, d_matrix)
-  bar = np.sum(foo, axis=0).reshape(n, 1)
-  foobar = 2 - np.sum(np.multiply(beta, foo), axis=0).reshape(n, 1)
+  n, foo = d_matrix.shape[1], np.divide(1, d_matrix)
+  foobar, bar = 2 - np.sum(np.multiply(beta, foo), axis=0).reshape(n, 1), np.sum(foo, axis=0).reshape(n, 1)
   alpha = np.divide(foobar, bar)
   return alpha
 
 
 def update_memberships(d_matrix, beta, alpha):
   C, n = d_matrix.shape[0], d_matrix.shape[1]
-  foo = np.tile(beta, (1, n))
-  bar = np.tile(alpha.reshape(1, n), (C, 1))
+  foo, bar = np.tile(beta, (1, n)), np.tile(alpha.reshape(1, n), (C, 1))
   memberships = np.multiply(np.divide(foo + bar, d_matrix), 0.5)
   return memberships
 
 
 def update_propotypes(memberships, points, m):
-  print("heloo")
-  return 1
+  foo, bar = np.dot(np.power(memberships, m), points), np.sum(np.power(memberships, m), axis=1).reshape(memberships.shape[0], 1)
+  prototypes = np.divide(foo, bar)
+  return prototypes
 
 
 def fcm_equalsize(points, C=2, threshold=0.01, m=2):
-  prototypes, n = np.random.random_sample((C, points.shape[1])), points.shape[0]
+  prototypes = np.random.random_sample((C, points.shape[1]))
   while False:
     d_matrix = spatial.distance_matrix(prototypes, points)
     beta = solve_beta(d_matrix)
     alpha = solve_alpha(d_matrix, beta)
     memberships = update_memberships(d_matrix, beta, alpha)
     prototypes = update_propotypes(memberships, points, m)
-  return memberships
-
+  return np.argmax(memberships, axis=0)
 
 if __name__ == "__main__":
-  d_matrix = np.array([[1, 2, 3], [5, 6, 7]])
-  beta = np.array([[2], [3]])
-  alpha = np.array([[1], [2], [3]])
-  C, n = d_matrix.shape[0], d_matrix.shape[1]
-  foo = np.tile(beta, (1, n))
-  bar = np.tile(alpha.reshape(1, n), (C, 1))
-  memberships = np.multiply(np.divide(foo + bar, d_matrix), 0.5)
-  print(memberships)
+  print("Hello World!")
+  
+  
+

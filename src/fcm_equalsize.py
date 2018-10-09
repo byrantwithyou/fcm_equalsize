987aea525211d690c33ab86eadcc2e2989717b61
byrantwithyou <byrantwithyou@163.com>
2018-10-10 05:29:09 +0800

    alpha beta memberships

--- src/fcm_equalsize.py
+++ src/fcm_equalsize.py
@@ -10,18 +10,34 @@ from numpy import fromfunction
 
 
 def solve_beta(d_matrix):
-  C = d_matrix.shape[0]
-  n = d_matrix.shape[1]
-  return 1
+  C, n = d_matrix.shape[0], d_matrix.shape[1]
+  foo = np.divide(1, np.dot(np.eye(C).repeat(C).reshape(C, C * C), np.divide(np.repeat(d_matrix, C, axis=0), np.tile(d_matrix, (C, 1)))))
+  bar = np.sum(foo, axis=1).reshape(C, 1)
+  b = n / C - bar
+  bar = np.diag(np.sum(np.divide(1, 2 * d_matrix), axis=1))
+  foobar = np.divide(1, np.tile(d_matrix.reshape(1, C * n), (C, 1)))
+  foobar = np.multiply(np.multiply(np.tile(foo, (1, C)), foobar), -1)
+  A = np.dot(foobar, np.repeat(np.eye(C), n, axis=0))
+  bar = np.diag(np.sum(np.divide(1, 2 * d_matrix), axis=1))
+  A = A - bar
+  return linalg.solve(A, b)
+
 
 def solve_alpha(d_matrix, beta):
-  print("solve_alpha")
-  return 1
+  n = d_matrix.shape[1]
+  foo = np.divide(1, d_matrix)
+  bar = np.sum(foo, axis=0).reshape(n, 1)
+  foobar = 2 - np.sum(np.multiply(beta, foo), axis=0).reshape(n, 1)
+  alpha = np.divide(foobar, bar)
+  return alpha
 
 
 def update_memberships(d_matrix, beta, alpha):
-  print("solve_dfs")
-  return 1
+  C, n = d_matrix.shape[0], d_matrix.shape[1]
+  foo = np.tile(beta, (1, n))
+  bar = np.tile(alpha.reshape(1, n), (C, 1))
+  memberships = np.multiply(np.divide(foo + bar, d_matrix), 0.5)
+  return memberships
 
 
 def update_propotypes(memberships, points, m):
@@ -41,20 +57,11 @@ def fcm_equalsize(points, C=2, threshold=0.01, m=2):
 
 
 if __name__ == "__main__":
-  d_matrix = np.array([[1, 2, 3], [4, 5, 6]])
-  #print(np.tile(d_matrix, (2, 1)))
-  #print(np.repeat(d_matrix, 2, axis=0))
-  a = np.repeat(d_matrix, 2, axis=0)
-  b = np.tile(d_matrix, (2, 1))
-  print(b.shape)
-  print(np.divide(a, b))
-  print(np.divide(1, ))
-  #print(np.divide(np.repeat(d_matrix, 2, axis=0)), np.tile(d_matrix, (2, 1)))
-  #a = np.array([[1, 2], [3, 4]])
-  #b = np.eye(3)
-  #print(np.repeat(b, 2).reshape((3, 6)))
-  #print(np.repeat(a, 2, axis=0))
-  
-  print()
-  #fcm_equalsize(points)  
-
+  d_matrix = np.array([[1, 2, 3], [5, 6, 7]])
+  beta = np.array([[2], [3]])
+  alpha = np.array([[1], [2], [3]])
+  C, n = d_matrix.shape[0], d_matrix.shape[1]
+  foo = np.tile(beta, (1, n))
+  bar = np.tile(alpha.reshape(1, n), (C, 1))
+  memberships = np.multiply(np.divide(foo + bar, d_matrix), 0.5)
+  print(memberships)

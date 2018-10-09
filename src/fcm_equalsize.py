0cbf64c69f9347a7f9d9892335b4dd253f3e47b3
byrantwithyou <byrantwithyou@163.com>
2018-10-09 09:25:06 +0800

    final initialization

--- src/fcm_equalsize.py
+++ src/fcm_equalsize.py
@@ -2,8 +2,15 @@
 # -*- coding:utf-8 -*-
 
 import numpy as np
+import scipy
+import scipy.spatial as spatial
 
-
-
+def fcm_equaisize(points, C=2, threshold=0.01):
+  d_matrix = spatial.distance_matrix(points, points)
+  
+if __name__ == "__main__":
+  points = np.random.random_sample((100, 2))
+  fcm_equaisize(points)
+  
 
 

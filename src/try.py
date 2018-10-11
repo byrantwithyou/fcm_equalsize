56461039904d8c8c9b428c7b66b7470e371f1695
byrantwithyou <byrantwithyou@163.com>
2018-10-11 19:43:55 +0800

    try.py okay

--- src/try.py
+++ src/try.py
@@ -1,4 +1,4 @@
-import numpy 
+import numpy as np
 import scipy
 from sklearn.datasets import make_blobs
 from scipy.spatial import distance_matrix
@@ -6,12 +6,12 @@ from scipy.spatial import distance_matrix
 
 centers = [[1, 1], [1.3, 1.1], [0.8, 0.9], [1.1, 0.], [.9, 1.1]]
 data, labels_true = make_blobs(centers=centers, n_samples=100)
-points = numpy.array(data)
-prototypes = numpy.array(centers)
-d_matrix = distance_matrix(prototypes, points) * 50
+points = np.array(data)
+prototypes = np.array(centers)
+d_matrix = distance_matrix(prototypes, points) 
 C = d_matrix.shape[0]
 n = d_matrix.shape[1]
-A = numpy.zeros((C, C))
+A = np.zeros((C, C))
 for k in range(C):
   for l in range (C):
     for j in range(n):
@@ -23,7 +23,7 @@ for k in range(C):
       if k == l:
         A[k][l] += .5 / d_matrix[k][j]
 
-b = numpy.zeros((C, 1))
+b = np.zeros((C, 1))
 b += n / C
 for k in range(C):
   for j in range(n):
@@ -31,4 +31,4 @@ for k in range(C):
     for i in range(C):
       foo += d_matrix[k][j] / d_matrix[i][j]
     b[k] += -1. / foo
-print(numpy.linalg.solve(A, b))
+print(np.linalg.solve(A, b))
